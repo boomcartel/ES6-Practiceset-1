3db41a2426d8ae6c41be3f386fee09b95e75462b
Rohit Turkar <rohittur2002@gmail.com>
2023-06-12 10:48:48 +0530

    Update question-1

--- question1.js
+++ question1.js
@@ -1,1 +1,5 @@
-console.log("Question-1")
+// Your ES6 code here
+const isEligibleToVote=age=>age>=18?"Eligible to vote":"Not Eligible to vote"
+console.log(isEligibleToVote(20)) // Eligible to vote
+console.log(isEligibleToVote(18)) // Eligible to vote
+console.log(isEligibleToVote(17)) // Not eligible to vote
